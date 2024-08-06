a0f78700b10c4c1f27fa297cd35544eb78711002
hwiem <40487668+hwiem@users.noreply.github.com>
2024-08-06 10:55:39 +0200

    feat(refs AB#14277): allow declining optional cookies instantly (3)
    * feat(refs DPLAN-12170): add first level decline button
    
    according to the gdpr, users should be able to decline
    optional cookies directly in the consent bar, without
    having to navigate somewhere else (previously: the
    settings view of the consent bar)
    
    * feat(refs DPLAN-12170): use outline buttons
    
    since we now have three buttons next to each other,
    and the 'accept' and 'decline' buttons need to have
    the same styling, we now use a more subtle styling
    for the buttons, so we don't have two solid primary
    buttons next to each other
    
    * feat(refs DPLAN-12170): allow passing html
    
    this way, we can include links in the notice
    (e.g., to the data protection entry)
    
    * style(refs DPLAN-12170): revert class renaming

--- src/js/MatomoConsent.js
+++ src/js/MatomoConsent.js
@@ -12,8 +12,9 @@ function createButton (textContent, id, type) {
 function createConsentSliderButtons () {
   var buttons = document.createElement('div')
   buttons.classList.add('dp-consent-slider__buttons')
-  buttons.appendChild(createButton('Alle akzeptieren', 'accept_all', 'primary'))
-  buttons.appendChild(createButton('Einstellungen ändern', 'change_settings'))
+  buttons.appendChild(createButton('Zustimmen', 'accept_all', 'primary'))
+  buttons.appendChild(createButton('Ablehnen', 'decline_optional', 'primary'))
+  buttons.appendChild(createButton('Überprüfen', 'change_settings'))
   return buttons
 }
 
@@ -62,7 +63,7 @@ function createNecessaryCookies (cookies) {
 
 function createNotice (notice) {
   var noticeContainer = document.createElement('div')
-  noticeContainer.appendChild(document.createTextNode(notice))
+  noticeContainer.innerHTML = notice
   return noticeContainer
 }
 
@@ -175,6 +176,12 @@ function dpConsent (config) {
       trackingConsent.given = true
       removeDpConsentElements()
     }
+
+    if (targetId === 'decline_optional') {
+      trackingConsent.given = false
+      removeDpConsentElements()
+    }
+
     if (targetId === 'change_settings') {
       showSettingsModal()
     }
